354c89c23f0008787fe80798beee3985235d59fc
Doris Luvizute <dorisluvizute@gmai.com>
2021-03-10 17:01:19 -0300

    Injeção de Dependencia

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,26 +1,26 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiomi Redmi Note 8");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/41G27clnK0L._AC_.jpg");
-		pedido.setUrlProduto("https://www.tudocelular.com/Redmi/fichas-tecnicas/n5716/Redmi-Note-8.html");
-		pedido.setDescricao("uma descrição qualquer para esse pedido");
+		List<Pedido> pedidos = pedidoRepository.recuperaTodosOsPedidos();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
